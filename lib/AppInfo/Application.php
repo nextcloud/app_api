8e8d1ab668aaf5a2e60df6332c125ef93b54d724
Andrey Borysenko <andrey18106x@gmail.com>
2023-07-24 21:28:16 +0300

    remove licence comments

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -2,33 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- *
- * Nextcloud - App Ecosystem V2
- *
- * @copyright Copyright (c) 2023 Andrey Borysenko <andrey18106x@gmail.com>
- *
- * @copyright Copyright (c) 2023 Alexander Piskun <bigcat88@icloud.com>
- *
- * @author 2023 Andrey Borysenko <andrey18106x@gmail.com>
- *
- * @license AGPL-3.0-or-later
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 namespace OCA\AppEcosystemV2\AppInfo;
 
 use OCA\AppEcosystemV2\Capabilities;
@@ -53,9 +26,6 @@ use Psr\Container\NotFoundExceptionInterface;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'app_ecosystem_v2';
-
-	public const CACHE_TTL = 60 * 60; // 1 hour
-	public const ICON_CACHE_TTL = 60 * 60 * 24; // 1 day
 
 	public function __construct(array $urlParams = []) {
 		parent::__construct(self::APP_ID, $urlParams);
