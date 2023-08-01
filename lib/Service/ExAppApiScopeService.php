d1e9b3a00f6a9a938afbd0f05d485c1782c371ab
Alexander Piskun <bigcat88@icloud.com>
2023-08-01 12:24:46 +0300

    added AE's `send notification` API route to scope
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- lib/Service/ExAppApiScopeService.php
+++ lib/Service/ExAppApiScopeService.php
@@ -81,6 +81,7 @@ class ExAppApiScopeService {
 			['api_route' => $aeApiV1Prefix . '/ex-app/preference', 'scope_group' => 1, 'name' => 'BASIC'],
 			['api_route' => $aeApiV1Prefix . '/users', 'scope_group' => 2, 'name' => 'SYSTEM'],
 			['api_route' => $aeApiV1Prefix . '/ex-app/all', 'scope_group' => 2, 'name' => 'SYSTEM'],
+			['api_route' => $aeApiV1Prefix . '/notification', 'scope_group' => 12, 'name' => 'NOTIFICATIONS'],
 
 			// Cloud scopes
 			['api_route' => '/cloud/capabilities', 'scope_group' => 1, 'name' => 'BASIC'],
