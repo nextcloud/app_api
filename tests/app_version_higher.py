8c295ac9c730fc95949f21de9a91486a03512353
Alexander Piskun <bigcat88@icloud.com>
2023-08-09 18:40:12 +0300

    fixed special test
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/app_version_higher.py
+++ tests/app_version_higher.py
@@ -5,7 +5,7 @@ from nc_py_api import Nextcloud, NextcloudApp, NextcloudException
 
 if __name__ == "__main__":
     nc_client = Nextcloud(nc_auth_user="admin", nc_auth_pass="admin")
-    assert nc_client.apps.is_disabled("nc_py_api") is False
+    assert nc_client.apps.ex_app_is_disabled("nc_py_api") is False
     nc_client.users.create("second_admin", password="2Very3Strong4", groups=["admin"])
 
     nc_application = NextcloudApp(user="admin")
@@ -16,11 +16,11 @@ if __name__ == "__main__":
         nc_application.users.get_details()  # this call should be rejected by AppEcosystem
     assert exc_info.value.status_code == 401
 
-    assert nc_client.apps.is_disabled("nc_py_api") is True
+    assert nc_client.apps.ex_app_is_disabled("nc_py_api") is True
     notifications = nc_client.users.notifications.get_all()
-    notification = [i for i in notifications if i.info.subject == "ex_app_version_update"]
+    notification = [i for i in notifications if i.object_type == "ex_app_update"]
     assert len(notification) == 1  # only one notification for each admin
     nc_client = Nextcloud(nc_auth_user="second_admin", nc_auth_pass="2Very3Strong4")
     notifications = nc_client.users.notifications.get_all()
-    notification = [i for i in notifications if i.info.subject == "ex_app_version_update"]
+    notification = [i for i in notifications if i.object_type == "ex_app_update"]
     assert len(notification) == 1  # only one notification for each admin
