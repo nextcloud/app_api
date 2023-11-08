5a2515c1493e7a68e48e01ee8484524288c08df5
Andrey Borysenko <andrey18106x@gmail.com>
2023-11-08 18:19:40 +0200

    fix missing spread operator
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- lib/Service/AppAPIService.php
+++ lib/Service/AppAPIService.php
@@ -619,7 +619,7 @@ class AppAPIService {
 				$exApp->getPort()) . $route;
 
 			if (is_array($options['headers'])) {
-				$options['headers'] = [...$options['headers'], $this->buildAppAPIAuthHeaders($request, $userId, $exApp)];
+				$options['headers'] = [...$options['headers'], ...$this->buildAppAPIAuthHeaders($request, $userId, $exApp)];
 			} else {
 				$options['headers'] = $this->buildAppAPIAuthHeaders($request, $userId, $exApp);
 			}
