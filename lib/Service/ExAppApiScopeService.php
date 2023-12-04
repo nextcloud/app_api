060d2a8b79841145f3747e406cb9199e75a8dbf8
Alexander Piskun <bigcat88@icloud.com>
2023-12-04 13:26:15 +0300

    reflect changes in api-scopes [ci skip]

--- lib/Service/ExAppApiScopeService.php
+++ lib/Service/ExAppApiScopeService.php
@@ -86,7 +86,7 @@ class ExAppApiScopeService {
 
 		$initApiScopes = [
 			// AppAPI scopes
-			['api_route' => $aeApiV1Prefix . '/files/actions/menu', 'scope_group' => 1, 'name' => 'BASIC', 'user_check' => 0],
+			['api_route' => $aeApiV1Prefix . '/ui/files-actions-menu', 'scope_group' => 1, 'name' => 'BASIC', 'user_check' => 0],
 			['api_route' => $aeApiV1Prefix . '/ui/top-menu', 'scope_group' => 1, 'name' => 'BASIC', 'user_check' => 0],
 			['api_route' => $aeApiV1Prefix . '/ui/initial-state', 'scope_group' => 1, 'name' => 'BASIC', 'user_check' => 0],
 			['api_route' => $aeApiV1Prefix . '/ui/script', 'scope_group' => 1, 'name' => 'BASIC', 'user_check' => 0],
