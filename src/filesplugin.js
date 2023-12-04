fb81d01e177cecb8e3ea3ca86370fe10df7ab96b
Andrey Borysenko <andrey18106x@gmail.com>
2023-12-04 21:57:51 +0200

    truncate leading slash in route for proxy
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- src/filesplugin.js
+++ src/filesplugin.js
@@ -26,6 +26,10 @@ function loadExAppInlineSvgIcon(appId, route) {
 }
 
 function generateAppAPIProxyUrl(appId, route) {
+	// truncate leading slash
+	if (route.startsWith('/')) {
+		route = route.substring(1)
+	}
 	return generateUrl(`/apps/app_api/proxy/${appId}/${route}`)
 }
 
