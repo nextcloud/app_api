bb135b878f742de2556bddff2322e99d98ef5c05
Andrey Borysenko <andrey18106x@gmail.com>
2023-12-15 14:56:16 +0200

    WIP: add test multi-select field, placeholders
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- lib/DeclarativeSettings/DeclarativeSettingsForm.php
+++ lib/DeclarativeSettings/DeclarativeSettingsForm.php
@@ -16,12 +16,22 @@ class DeclarativeSettingsForm implements IDeclarativeSettingsForm {
 			'storage_type' => 'external',
 			'title' => 'AppAPI declarative settings', // NcSettingsSection name
 			'description' => 'These fields are rendered dynamically from declarative schema', // NcSettingsSection description
+			'doc_url' => '', // NcSettingsSection doc_url for documentation, empty string if not needed
 			'fields' => [
+				[
+					'id' => 'test_ex_app_field_7',
+					'title' => 'Selection',
+					'description' => 'Select some option setting',
+					'type' => 'multi-select', // select, radio, multi-select
+					'options' => ['foo', 'bar', 'baz'],
+					'placeholder' => 'Select some multiple options',
+				],
 				[
 					'id' => 'some_real_setting',
 					'title' => 'Choose init status check background job interval',
 					'description' => 'How often AppAPI should check for initialization status',
 					'type' => 'radio', // radio, radio-button (NcCheckboxRadioSwitch button-variant)
+					'placeholder' => 'Choose init status check background job interval',
 					'options' => [
 						[
 							'name' => 'Each 40 minutes', // NcCheckboxRadioSwitch display name
@@ -46,36 +56,42 @@ class DeclarativeSettingsForm implements IDeclarativeSettingsForm {
 					'title' => 'Default text field', // label
 					'description' => 'Set some simple text setting', // hint
 					'type' => 'text', // text, password, email, tel, url, number
+					'placeholder' => 'Enter text setting', // placeholder
 				],
 				[
 					'id' => 'test_ex_app_field_1_1',
 					'title' => 'Email field',
 					'description' => 'Set email config',
 					'type' => 'email',
+					'placeholder' => 'Enter email',
 				],
 				[
 					'id' => 'test_ex_app_field_1_2',
 					'title' => 'Tel field',
 					'description' => 'Set tel config',
 					'type' => 'tel',
+					'placeholder' => 'Enter your tel',
 				],
 				[
 					'id' => 'test_ex_app_field_1_3',
 					'title' => 'Url (website) field',
 					'description' => 'Set url config',
 					'type' => 'url',
+					'placeholder' => 'Enter url',
 				],
 				[
 					'id' => 'test_ex_app_field_1_4',
 					'title' => 'Number field',
 					'description' => 'Set number config',
 					'type' => 'number',
+					'placeholder' => 'Enter number value',
 				],
 				[
 					'id' => 'test_ex_app_field_2',
 					'title' => 'Password',
 					'description' => 'Set some secure value setting',
 					'type' => 'password',
+					'placeholder' => 'Set secure value',
 				],
 				[
 					'id' => 'test_ex_app_field_3',
@@ -83,6 +99,7 @@ class DeclarativeSettingsForm implements IDeclarativeSettingsForm {
 					'description' => 'Select some option setting',
 					'type' => 'select', // select, radio, multi-select
 					'options' => ['foo', 'bar', 'baz'],
+					'placeholder' => 'Select some option setting',
 				],
 				[
 					'id' => 'test_ex_app_field_4',
@@ -96,11 +113,10 @@ class DeclarativeSettingsForm implements IDeclarativeSettingsForm {
 					'title' => 'Multiple checkbox toggles, describing one setting, checked options are saved as an JSON object {foo: true, bar: false}',
 					'description' => 'Select checkbox option setting',
 					'type' => 'multi-checkbox', // checkbox, multi-checkbox
-					'label' => 'Select multiple toggles',
 					'options' => [
 						[
 							'name' => 'Foo',
-							'value' => 'foo',
+							'value' => 'foo', // multiple-checkbox configkey
 						],
 						[
 							'name' => 'Bar',
