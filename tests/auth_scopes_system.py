46be794e03cbe2010df092dfde0019e3343ae3a4
Alexander Piskun <bigcat88@icloud.com>
2024-01-01 13:33:22 +0300

    auth tests for "ALL" scope (3)

--- tests/auth_scopes_system.py
+++ tests/auth_scopes_system.py
@@ -7,9 +7,10 @@ if __name__ == "__main__":
     nc = nc_py_api.NextcloudApp(user="admin")
     assert nc.capabilities
     if int(sys.argv[1]) == 0:
-        assert nc.ocs("GET", "/ocs/v2.php/core/whatsnew")
+        nc.ocs("GET", "/ocs/v2.php/core/whatsnew")
     else:
         with pytest.raises(nc_py_api.NextcloudException) as e:
-            assert e.value.status_code == 401
+			nc.ocs("GET", "/ocs/v2.php/core/whatsnew")
+        assert e.value.status_code == 401
     assert nc.users_list()
     exit(0)
