e5660ee3d827ab5869850c940c27293d1f1174e6
Alexander Piskun <bigcat88@icloud.com>
2024-01-01 12:30:00 +0300

    adjusted "no-init" test (3)

---
+++ tests/auth_scopes_system.py
@@ -0,0 +1,15 @@
+import sys
+import pytest
+import nc_py_api
+
+
+if __name__ == "__main__":
+    nc = nc_py_api.NextcloudApp()
+    assert nc.capabilities
+    if int(sys.argv[1]) == 0:
+        assert nc.ocs("GET", "/ocs/v2.php/core/whatsnew")
+    else:
+        with pytest.raises(nc_py_api.NextcloudException) as e:
+            assert e.value.status_code == 401
+    assert nc.users_list()
+    exit(0)
