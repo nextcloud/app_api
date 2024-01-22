0e06538d06e091f367fece56012e6c670151a9b5
Alexander Piskun <bigcat88@icloud.com>
2024-01-22 11:46:56 +0300

    removed caching for daemons [skip ci]
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- lib/Service/DaemonConfigService.php
+++ lib/Service/DaemonConfigService.php
@@ -4,30 +4,22 @@ declare(strict_types=1);
 
 namespace OCA\AppAPI\Service;
 
-use OCA\AppAPI\AppInfo\Application;
 use OCA\AppAPI\Db\DaemonConfig;
 use OCA\AppAPI\Db\DaemonConfigMapper;
 
 use OCP\AppFramework\Db\DoesNotExistException;
 use OCP\AppFramework\Db\MultipleObjectsReturnedException;
 use OCP\DB\Exception;
-use OCP\ICache;
-use OCP\ICacheFactory;
 use Psr\Log\LoggerInterface;
 
 /**
  * Daemon configuration (daemons)
  */
 class DaemonConfigService {
-	public const CACHE_TTL = 60 * 60 * 2; // 2 hours
-	private ICache $cache;
-
 	public function __construct(
 		private readonly LoggerInterface    $logger,
-		ICacheFactory                       $cacheFactory,
 		private readonly DaemonConfigMapper $mapper,
 	) {
-		$this->cache = $cacheFactory->createDistributed(Application::APP_ID . '/daemon_configs');
 	}
 
 	public function registerDaemonConfig(array $params): ?DaemonConfig {
@@ -39,9 +31,13 @@ class DaemonConfigService {
 				return null;
 			}
 		}
+		if ($params['protocol'] !== 'http' && $params['protocol'] !== 'https') {
+			$this->logger->error('Failed to register daemon configuration. `protocol` must be `http` or `https`.');
+			return null;
+		}
 		$params['deploy_config']['nextcloud_url'] = rtrim($params['deploy_config']['nextcloud_url'], '/');
 		try {
-			$daemonConfig = $this->mapper->insert(new DaemonConfig([
+			return $this->mapper->insert(new DaemonConfig([
 				'name' => $params['name'],
 				'display_name' => $params['display_name'],
 				'accepts_deploy_id' => $params['accepts_deploy_id'],
@@ -49,8 +45,6 @@ class DaemonConfigService {
 				'host' => $params['host'],
 				'deploy_config' => $params['deploy_config'],
 			]));
-			$this->cache->remove('/daemon_configs');
-			return $daemonConfig;
 		} catch (Exception $e) {
 			$this->logger->error('Failed to register daemon config. Error: ' . $e->getMessage(), ['exception' => $e]);
 			return null;
@@ -59,10 +53,7 @@ class DaemonConfigService {
 
 	public function unregisterDaemonConfig(DaemonConfig $daemonConfig): ?DaemonConfig {
 		try {
-			$daemonConfig = $this->mapper->delete($daemonConfig);
-			$this->cache->remove('/daemon_configs');
-			$this->cache->remove('/daemon_config_' . $daemonConfig->getName());
-			return $daemonConfig;
+			return $this->mapper->delete($daemonConfig);
 		} catch (Exception $e) {
 			$this->logger->error('Failed to unregister daemon config. Error: ' . $e->getMessage(), ['exception' => $e]);
 			return null;
@@ -74,17 +65,7 @@ class DaemonConfigService {
 	 */
 	public function getRegisteredDaemonConfigs(): array {
 		try {
-			$cacheKey = '/daemon_configs';
-			$cached = $this->cache->get($cacheKey);
-			if ($cached !== null) {
-				return array_map(function ($cachedEntry) {
-					return $cachedEntry instanceof DaemonConfig ? $cachedEntry : new DaemonConfig($cachedEntry);
-				}, $cached);
-			}
-
-			$daemonConfigs = $this->mapper->findAll();
-			$this->cache->set($cacheKey, $daemonConfigs, self::CACHE_TTL);
-			return $daemonConfigs;
+			return $this->mapper->findAll();
 		} catch (Exception $e) {
 			$this->logger->debug('Failed to get registered daemon configs. Error: ' . $e->getMessage(), ['exception' => $e]);
 			return [];
@@ -93,15 +74,7 @@ class DaemonConfigService {
 
 	public function getDaemonConfigByName(string $name): ?DaemonConfig {
 		try {
-			$cacheKey = '/daemon_config_' . $name;
-			$cached = $this->cache->get($cacheKey);
-			if ($cached !== null) {
-				return $cached instanceof DaemonConfig ? $cached : new DaemonConfig($cached);
-			}
-
-			$daemonConfig = $this->mapper->findByName($name);
-			$this->cache->set($cacheKey, $daemonConfig, self::CACHE_TTL);
-			return $daemonConfig;
+			return $this->mapper->findByName($name);
 		} catch (DoesNotExistException|MultipleObjectsReturnedException|Exception $e) {
 			$this->logger->error('Failed to get daemon config by name. Error: ' . $e->getMessage(), ['exception' => $e]);
 			return null;
@@ -110,10 +83,7 @@ class DaemonConfigService {
 
 	public function updateDaemonConfig(DaemonConfig $daemonConfig): ?DaemonConfig {
 		try {
-			$cacheKey = '/daemon_config_' . $daemonConfig->getName();
-			$daemonConfig = $this->mapper->update($daemonConfig);
-			$this->cache->set($cacheKey, $daemonConfig, self::CACHE_TTL);
-			return $daemonConfig;
+			return $this->mapper->update($daemonConfig);
 		} catch (Exception $e) {
 			$this->logger->error('Failed to update DaemonConfig. Error: ' . $e->getMessage(), ['exception' => $e]);
 			return null;
