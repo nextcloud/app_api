0fb27f26f713931285d1b7f0795573945c438621
Alexander Piskun <bigcat88@icloud.com>
2024-01-30 19:35:30 +0300

    removed `api scopes` from capabilities [ci skip]
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -5,23 +5,16 @@ declare(strict_types=1);
 namespace OCA\AppAPI;
 
 use OCA\AppAPI\AppInfo\Application;
-use OCA\AppAPI\Db\ExAppScope;
-use OCA\AppAPI\Service\ExAppScopesService;
-use OCA\AppAPI\Service\ExAppService;
 use OCA\AppAPI\Service\ProvidersAI\TextProcessingService;
 use OCP\App\IAppManager;
 use OCP\Capabilities\ICapability;
 use OCP\IConfig;
-use OCP\IRequest;
 
 class Capabilities implements ICapability {
 
 	public function __construct(
-		private IConfig            $config,
-		private IAppManager        $appManager,
-		private ExAppService       $service,
-		private ExAppScopesService $exAppScopesService,
-		private IRequest           $request,
+		private readonly IConfig     $config,
+		private readonly IAppManager $appManager,
 	) {
 	}
 
@@ -33,21 +26,8 @@ class Capabilities implements ICapability {
 				'task_types' => array_keys(TextProcessingService::TASK_TYPES),
 			]
 		];
-		$this->attachExAppScopes($capabilities);
 		return [
 			'app_api' => $capabilities,
 		];
-	}
-
-	private function attachExAppScopes(&$capabilities): void {
-		$appId = $this->request->getHeader('EX-APP-ID');
-		if ($appId !== '') {
-			$exApp = $this->service->getExApp($appId);
-			if ($exApp !== null) {
-				$capabilities['scopes'] = array_map(function (ExAppScope $scope) {
-					return intval($scope->getScopeGroup());
-				}, $this->exAppScopesService->getExAppScopes($exApp));
-			}
-		}
 	}
 }
