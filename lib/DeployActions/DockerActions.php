848f0705b0ed8a42ecdf9e9f40cad2d2e75274f4
Alexander Piskun <bigcat88@icloud.com>
2024-02-19 15:11:31 +0300

    workaround for "stream" and new pullImage implementation
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- lib/DeployActions/DockerActions.php
+++ lib/DeployActions/DockerActions.php
@@ -38,6 +38,7 @@ class DockerActions implements IDeployActions {
 	public const APP_API_HAPROXY_USER = 'app_api_haproxy_user';
 
 	private Client $guzzleClient;
+	private bool $useSocket = false;  # for `pullImage` function, to detect can be stream used or not.
 
 	public function __construct(
 		private readonly LoggerInterface     $logger,
@@ -207,63 +208,69 @@ class DockerActions implements IDeployActions {
 		$url = $this->buildApiUrl($dockerUrl, sprintf('images/create?fromImage=%s', urlencode($imageId)));
 		$this->logger->info(sprintf('Pulling ExApp Image: %s', $imageId));
 		try {
-			$response = $this->guzzleClient->post($url);
-//			$response = $this->guzzleClient->post($url, ['stream' => true]);
+			if ($this->useSocket) {
+				$response = $this->guzzleClient->post($url);
+			} else {
+				$response = $this->guzzleClient->post($url, ['stream' => true]);
+			}
 			if ($response->getStatusCode() !== 200) {
 				return sprintf('Pulling ExApp Image: %s return status code: %d', $imageId, $response->getStatusCode());
 			}
+			if ($this->useSocket) {
+				return '';
+			}
+			$lastPercent = $startPercent;
+			$layers = [];
+			$buffer = '';
+			$responseBody = $response->getBody();
+			while (!$responseBody->eof()) {
+				$buffer .= $responseBody->read(1024);
+				try {
+					while (($newlinePos = strpos($buffer, "\n")) !== false) {
+						$line = substr($buffer, 0, $newlinePos);
+						$buffer = substr($buffer, $newlinePos + 1);
+						$jsonLine = json_decode(trim($line));
+						if ($jsonLine) {
+							if (isset($jsonLine->id) && isset($jsonLine->status)) {
+								$layerId = $jsonLine->id;
+								$status = strtolower($jsonLine->status);
+								foreach ($layerInProgress as $substring) {
+									if (str_contains($status, $substring)) {
+										$layers[$layerId] = false;
+										break;
+									}
+								}
+								foreach ($layerFinished as $substring) {
+									if (str_contains($status, $substring)) {
+										$layers[$layerId] = true;
+										break;
+									}
+								}
+							}
+						} else {
+							$this->logger->warning(
+								sprintf("Progress tracking of image pulling(%s) disabled, error: %d, data: %s", $exApp->getAppid(), json_last_error(), $line)
+							);
+							$disableProgressTracking = true;
+						}
+					}
+				} catch (Exception $e) {
+					$this->logger->warning(
+						sprintf("Progress tracking of image pulling(%s) disabled, exception: %s", $exApp->getAppid(), $e->getMessage()), ['exception' => $e]
+					);
+					$disableProgressTracking = true;
+				}
+				if (!$disableProgressTracking) {
+					$completedLayers = count(array_filter($layers));
+					$totalLayers = count($layers);
+					$newLastPercent = intval($totalLayers > 0 ? ($completedLayers / $totalLayers) * ($maxPercent - $startPercent) : 0);
+					if ($lastPercent != $newLastPercent) {
+						$this->exAppService->setAppDeployProgress($exApp, $newLastPercent);
+						$lastPercent = $newLastPercent;
+					}
+				}
+			}
 			return '';
-//			$lastPercent = $startPercent;
-//			$layers = [];
-//			$buffer = '';
-//			while (!$response->getBody()->eof()) {
-//				$buffer .= $response->getBody()->read(1024);
-//				try {
-//					while (($newlinePos = strpos($buffer, "\n")) !== false) {
-//						$line = substr($buffer, 0, $newlinePos);
-//						$buffer = substr($buffer, $newlinePos + 1);
-//						$jsonLine = json_decode(trim($line));
-//						if ($jsonLine) {
-//							if (isset($jsonLine->id) && isset($jsonLine->status)) {
-//								$layerId = $jsonLine->id;
-//								$status = strtolower($jsonLine->status);
-//								foreach ($layerInProgress as $substring) {
-//									if (str_contains($status, $substring)) {
-//										$layers[$layerId] = false;
-//										break;
-//									}
-//								}
-//								foreach ($layerFinished as $substring) {
-//									if (str_contains($status, $substring)) {
-//										$layers[$layerId] = true;
-//										break;
-//									}
-//								}
-//							}
-//						} else {
-//							$this->logger->warning(
-//								sprintf("Progress tracking of image pulling(%s) disabled, error: %d, data: %s", $exApp->getAppid(), json_last_error(), $line)
-//							);
-//							$disableProgressTracking = true;
-//						}
-//					}
-//				} catch (Exception $e) {
-//					$this->logger->warning(
-//						sprintf("Progress tracking of image pulling(%s) disabled, exception: %s", $exApp->getAppid(), $e->getMessage()), ['exception' => $e]
-//					);
-//					$disableProgressTracking = true;
-//				}
-//				if (!$disableProgressTracking) {
-//					$completedLayers = count(array_filter($layers));
-//					$totalLayers = count($layers);
-//					$newLastPercent = intval($totalLayers > 0 ? ($completedLayers / $totalLayers) * ($maxPercent - $startPercent) : 0);
-//					if ($lastPercent != $newLastPercent) {
-//						$this->exAppService->setAppDeployProgress($exApp, $newLastPercent);
-//						$lastPercent = $newLastPercent;
-//					}
-//				}
-//			}
-//			return '';
 		} catch (GuzzleException $e) {
 			$this->logger->error('Failed to pull image', ['exception' => $e]);
 			error_log($e->getMessage());
@@ -516,6 +523,7 @@ class DockerActions implements IDeployActions {
 					CURLOPT_UNIX_SOCKET_PATH => $daemonConfig->getHost(),
 				],
 			];
+			$this->useSocket = true;
 		} elseif ($daemonConfig->getProtocol() === 'https') {
 			$guzzleParams = $this->setupCerts($guzzleParams);
 		}
