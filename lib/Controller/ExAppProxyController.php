80878b1f8b50d7c47a0b694dcaf502059693c8eb
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-06-10 20:40:51 +0300

    UIProxy: do not cache "application/x-tar" mimetype
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- lib/Controller/ExAppProxyController.php
+++ lib/Controller/ExAppProxyController.php
@@ -66,7 +66,8 @@ class ExAppProxyController extends Controller {
 
 		$proxyResponse = new ProxyResponse($response->getStatusCode(), $responseHeaders, $content);
 		if ($cache && !$isHTML && empty($response->getHeader('cache-control'))
-			&& $response->getHeader('Content-Type') !== 'application/json') {
+			&& $response->getHeader('Content-Type') !== 'application/json'
+			&& $response->getHeader('Content-Type') !== 'application/x-tar') {
 			$proxyResponse->cacheFor(3600);
 		}
 		return $proxyResponse;
