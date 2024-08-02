c0decca10d492793c018e49632a2823caf16ed9e
Andrey Borysenko <andrey18106x@gmail.com>
2024-08-02 17:15:59 +0300

    enh(ui): use NcPasswordField for haproxy_password
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- src/components/DaemonConfig/DaemonConfigDetailsModal.vue
+++ src/components/DaemonConfig/DaemonConfigDetailsModal.vue
@@ -20,9 +20,15 @@
 				<h3>{{ t('app_api', 'Deploy config') }}</h3>
 				<p><b>{{ t('app_api', 'Docker network') }}: </b>{{ daemon.deploy_config.net }}</p>
 				<p><b>{{ t('app_api', 'Nextcloud URL') }}: </b>{{ daemon.deploy_config.nextcloud_url }}</p>
-				<p v-if="daemon.deploy_config.haproxy_password">
-					<b>{{ t('app_api', 'HaProxy password') }}: </b>
-					{{ daemon.deploy_config?.haproxy_password }}
+				<p v-if="daemon.deploy_config.haproxy_password" class="external-label">
+					<label for="haproxy_password"><b>{{ t('app_api', 'HaProxy password') }}: </b></label>
+					<NcPasswordField
+						id="haproxy_password"
+						:value="daemon.deploy_config?.haproxy_password"
+						:disable="true"
+						style="width: fit-content;"
+						readonly
+						autocomplete="off" />
 				</p>
 				<p>
 					<b>{{ t('app_api', 'GPUs support') }}:</b> {{ daemon.deploy_config.computeDevice && daemon.deploy_config?.computeDevice?.id !== 'cpu' || false }}
@@ -60,6 +66,7 @@ import { showSuccess, showError } from '@nextcloud/dialogs'
 import NcModal from '@nextcloud/vue/dist/Components/NcModal.js'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
 import NcNoteCard from '@nextcloud/vue/dist/Components/NcNoteCard.js'
+import NcPasswordField from '@nextcloud/vue/dist/Components/NcPasswordField.js'
 
 import NcLoadingIcon from '@nextcloud/vue/dist/Components/NcLoadingIcon.js'
 import Connection from 'vue-material-design-icons/Connection.vue'
@@ -71,6 +78,7 @@ export default {
 		NcButton,
 		NcNoteCard,
 		NcLoadingIcon,
+		NcPasswordField,
 		Connection,
 	},
 	props: {
@@ -129,5 +137,15 @@ export default {
 	display: flex;
 	justify-content: space-between;
 	margin: 20px 0;
+}
+
+.external-label {
+	display: flex;
+	align-items: center;
+	width: 100%;
+
+	label {
+		margin-right: 5px;
+	}
 }
 </style>
