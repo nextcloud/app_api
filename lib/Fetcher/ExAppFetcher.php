91b89968bc5d9929701c349b8906f668db455022
Andrey Borysenko <andrey18106x@gmail.com>
2024-10-09 21:01:26 +0300

    chore: add public getExAppsWithUpdate for settings, minor fixes
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- lib/Fetcher/ExAppFetcher.php
+++ lib/Fetcher/ExAppFetcher.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace OCA\AppAPI\Fetcher;
 
-use Exception;
 use InvalidArgumentException;
 use OC\App\AppStore\Version\VersionParser;
 use OC\App\CompareVersion;
 use OC\Files\AppData\Factory;
+use OCA\AppAPI\Service\ExAppService;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\Http\Client\IClientService;
 use OCP\IConfig;
@@ -158,5 +158,16 @@ class ExAppFetcher extends AppAPIFetcher {
 		}
 
 		return $apps;
+	}
+
+	public function getExAppsWithUpdates(): array {
+		$apps = $this->get();
+		$appsWithUpdates = array_filter($apps, function (array $app) {
+			$exAppService = Server::get(ExAppService::class);
+			$exApp = $exAppService->getExApp($app['id']);
+			$newestVersion = $app['releases'][0]['version'];
+			return $exApp !== null && isset($app['releases'][0]['version']) && version_compare($newestVersion, $exApp->getVersion(), '>');
+		});
+		return array_values($appsWithUpdates);
 	}
 }
