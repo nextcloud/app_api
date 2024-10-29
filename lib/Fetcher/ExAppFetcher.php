b8200b70b5e5eba9beedd6d0e89d6d128c3f6062
Andrey Borysenko <andrey18106x@gmail.com>
2024-10-29 21:26:46 +0200

    chore: sync changes for Apps management migration to settings (#417)
    This PR synchronizes backend logic for ExAppFetcher and required changes
    for Apps management.
    
    ![Screen Shot 2024-10-14 at 17 01
    48](https://github.com/user-attachments/assets/60667041-92b1-4d9e-a78a-9476a1271d1f)
    
    ## TODO
    
    - [ ] Merge with the server PR for settings app changes:
    https://github.com/nextcloud/server/pull/48665
    - [x] Remove old UI parts that are not needed anymore
    
    ---------
    
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>
    Co-authored-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- lib/Fetcher/ExAppFetcher.php
+++ lib/Fetcher/ExAppFetcher.php
@@ -4,20 +4,19 @@ declare(strict_types=1);
 
 namespace OCA\AppAPI\Fetcher;
 
-use Exception;
 use InvalidArgumentException;
 use OC\App\AppStore\Version\VersionParser;
 use OC\App\CompareVersion;
 use OC\Files\AppData\Factory;
+use OCA\AppAPI\Service\ExAppService;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\Http\Client\IClientService;
 use OCP\IConfig;
-use OCP\ServerVersion;
+use OCP\Server;
 use OCP\Support\Subscription\IRegistry;
 use Psr\Log\LoggerInterface;
 
 class ExAppFetcher extends AppAPIFetcher {
-	private CompareVersion $compareVersion;
 	private bool $ignoreMaxVersion;
 
 	public function __construct(
@@ -25,10 +24,9 @@ class ExAppFetcher extends AppAPIFetcher {
 		IClientService $clientService,
 		ITimeFactory $timeFactory,
 		IConfig $config,
-		CompareVersion $compareVersion,
+		private CompareVersion $compareVersion,
 		LoggerInterface $logger,
 		protected IRegistry $registry,
-		protected ServerVersion $serverVersion,
 	) {
 		parent::__construct(
 			$appDataFactory,
@@ -37,10 +35,7 @@ class ExAppFetcher extends AppAPIFetcher {
 			$config,
 			$logger,
 			$registry,
-			$serverVersion
 		);
-
-		$this->compareVersion = $compareVersion;
 
 		$this->fileName = 'appapi_apps.json';
 		$this->endpointName = 'appapi_apps.json';
@@ -49,14 +44,13 @@ class ExAppFetcher extends AppAPIFetcher {
 
 	/**
 	 * Only returns the latest compatible app release in the releases array
-	 *
-	 * @throws Exception
 	 */
 	protected function fetch(string $ETag, string $content, bool $allowUnstable = false): array {
 		/** @var mixed[] $response */
 		$response = parent::fetch($ETag, $content);
 
-		if (empty($response)) {
+		if (!isset($response['data']) || $response['data'] === null) {
+			$this->logger->warning('Response from appstore is invalid, ExApps could not be retrieved. Try again later.', ['app' => 'appstoreExAppFetcher']);
 			return [];
 		}
 
@@ -150,6 +144,9 @@ class ExAppFetcher extends AppAPIFetcher {
 		$allowPreReleases = $allowUnstable || $this->getChannel() === 'beta' || $this->getChannel() === 'daily' || $this->getChannel() === 'git';
 
 		$apps = parent::get($allowPreReleases);
+		if (empty($apps)) {
+			return [];
+		}
 		$allowList = $this->config->getSystemValue('appsallowlist');
 
 		// If the admin specified a allow list, filter apps from the appstore
@@ -160,5 +157,16 @@ class ExAppFetcher extends AppAPIFetcher {
 		}
 
 		return $apps;
+	}
+
+	public function getExAppsWithUpdates(): array {
+		$apps = $this->get();
+		$appsWithUpdates = array_filter($apps, function (array $app) {
+			$exAppService = Server::get(ExAppService::class);
+			$exApp = $exAppService->getExApp($app['id']);
+			$newestVersion = $app['releases'][0]['version'];
+			return $exApp !== null && isset($app['releases'][0]['version']) && version_compare($newestVersion, $exApp->getVersion(), '>');
+		});
+		return array_values($appsWithUpdates);
 	}
 }
