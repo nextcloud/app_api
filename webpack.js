91fbc02c009f363c8598992a3abc4474705a6ec7
Andy Scherzinger <info@andy-scherzinger.de>
2024-12-16 16:50:35 +0100

    build(reuse): Add license file generation
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- webpack.js
+++ webpack.js
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 const ESLintPlugin = require('eslint-webpack-plugin')
@@ -36,5 +41,33 @@ webpackConfig.plugins.push(
 		failOnError: !isDev,
 	}),
 )
+
+// Generate reuse license files if not in development mode
+if (!isDev) {
+	const WebpackSPDXPlugin = require('./build-js/WebpackSPDXPlugin.js')
+	webpackConfig.plugins.push(new WebpackSPDXPlugin({
+		override: {
+			select2: 'MIT',
+		},
+	}))
+
+	webpackConfig.optimization.minimizer = [{
+		apply: (compiler) => {
+			// Lazy load the Terser plugin
+			const TerserPlugin = require('terser-webpack-plugin')
+			new TerserPlugin({
+				extractComments: false,
+				terserOptions: {
+					format: {
+						comments: false,
+					},
+					compress: {
+						passes: 2,
+					},
+				},
+		  }).apply(compiler)
+		},
+	}]
+}
 
 module.exports = webpackConfig
