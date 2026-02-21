1d5ad0f181c9995dbd62ebccc88bf9dbd770c1d6
Isaac Lee <isaac.wonha.lee@outlook.com>
2026-02-21 14:22:56 -0500

    feat(daemon): add --show-deploy-config flag to daemon:list command
    Signed-off-by: Isaac Lee <isaac.wonha.lee@outlook.com>

--- lib/Command/Daemon/ListDaemons.php
+++ lib/Command/Daemon/ListDaemons.php
@@ -16,6 +16,7 @@ use OCP\IAppConfig;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ListDaemons extends Command {
@@ -30,6 +31,7 @@ class ListDaemons extends Command {
 	protected function configure(): void {
 		$this->setName('app_api:daemon:list');
 		$this->setDescription('List registered daemons');
+		$this->addOption('output', 'o', InputOption::VALUE_REQUIRED, 'Output format (json, table)', 'table');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
@@ -41,27 +43,59 @@ class ListDaemons extends Command {
 
 		$defaultDaemonName = $this->appConfig->getValueString(Application::APP_ID, 'default_daemon_config', lazy: true);
 
-		$output->writeln('Registered ExApp daemon configs:');
-		$table = new Table($output);
-		$table->setHeaders(['Def', 'Name', 'Display name', 'Deploy ID', 'Protocol', 'Host', 'NC Url', 'Is HaRP', 'HaRP FRP Address', 'HaRP Docker Socket Port']);
-		$rows = [];
-
-		foreach ($daemonConfigs as $daemon) {
-			$rows[] = [
-				$daemon->getName() === $defaultDaemonName ? '*' : '',
-				$daemon->getName(), $daemon->getDisplayName(),
-				$daemon->getAcceptsDeployId(),
-				$daemon->getProtocol(),
-				$daemon->getHost(),
-				$daemon->getDeployConfig()['nextcloud_url'],
-				isset($daemon->getDeployConfig()['harp']) ? 'yes' : 'no',
-				$daemon->getDeployConfig()['harp']['frp_address'] ?? '(none)',
-				$daemon->getDeployConfig()['harp']['docker_socket_port'] ?? '(none)',
-			];
+		$outputFormat = $input->getOption('output');
+		if (!in_array($outputFormat, ['json', 'table'], true)) {
+			$output->writeln(sprintf('<error>Invalid output format "%s". Valid options are: json, table</error>', $outputFormat));
+			return 1;
 		}
 
-		$table->setRows($rows);
-		$table->render();
+		$output->writeln('Registered ExApp daemon configs:');
+
+		if ($outputFormat === 'json') {
+			$allDaemonInfo = [];
+			foreach ($daemonConfigs as $daemon) {
+				$deployConfig = $daemon->getDeployConfig();
+
+				if (isset($deployConfig['haproxy_password'])) {
+					$deployConfig['haproxy_password'] = '***';
+				}
+
+				$allDaemonInfo[] = [
+					'name' => $daemon->getName(),
+					'display_name' => $daemon->getDisplayName(),
+					'deploy_id' => $daemon->getAcceptsDeployId(),
+					'protocol' => $daemon->getProtocol(),
+					'host' => $daemon->getHost(),
+					'deploy_config' => $deployConfig,
+				];
+
+			}
+			$output->writeln(json_encode($allDaemonInfo, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+		} else {
+			$table = new Table($output);
+			$headers = ['Def', 'Name', 'Display name', 'Deploy ID', 'Protocol', 'Host', 'NC Url', 'Is HaRP', 'HaRP FRP Address', 'HaRP Docker Socket Port'];
+			$table->setHeaders($headers);
+
+			$rows = [];
+			foreach ($daemonConfigs as $daemon) {
+				$deployConfig = $daemon->getDeployConfig();
+				$rows[] = [
+					$daemon->getName() === $defaultDaemonName ? '*' : '',
+					$daemon->getName(),
+					$daemon->getDisplayName(),
+					$daemon->getAcceptsDeployId(),
+					$daemon->getProtocol(),
+					$daemon->getHost(),
+					$deployConfig['nextcloud_url'],
+					isset($deployConfig['harp']) ? 'yes' : 'no',
+					$deployConfig['harp']['frp_address'] ?? '(none)',
+					$deployConfig['harp']['docker_socket_port'] ?? '(none)',
+				];
+			}
+
+			$table->setRows($rows);
+			$table->render();
+		}
 
 		return 0;
 	}
